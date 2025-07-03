292bdb1c06ba87024b41f154572b1310f940efc6
Mendoul <mendoul@yaels-imac.mynetworksettings.com>
2025-07-03 10:59:53 -0400

    updated maxValue

--- maxValue.ts
+++ maxValue.ts
@@ -24,8 +24,3 @@ const maxValue = (array) => {
   // return max
   return max;
 };
-
-console.log(maxValue([4, 7, 2, 8, 10, 9]))
-console.log(maxValue([-5, -2, -1, -11]))
-console.log(maxValue([42]))
-console.log(maxValue([2, 5, 1, 1, 4]))
